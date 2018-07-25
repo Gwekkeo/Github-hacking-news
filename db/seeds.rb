cd9360ad9590ab9940f9d51f8374040c844d9f71
Gwekkeo <gwendoline.cassand@gmail.com>
2018-07-26 00:28:53 +0200

    mise a jour seed affichage

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,36 @@
 require 'faker'
 
-#Ajout user dans table
+puts "Ajout user dans table"
 10.times do
  	User.create(name: Faker::DrWho.actor)
 end
 
-#Ajout lien table
+puts "Ajout lien table"
 10.times do
  	Link.create(title: Faker::Color.color_name, url: Faker::Internet.url)
 end
 
-#Ajout contenu comments
+puts "Ajout contenu comments"
 5.times do
 	Comment.create(content: Faker::ChuckNorris.fact)
 end
 
-#Ajout contenu subcomments
+puts "Ajout contenu subcomments"
 12.times do
 	Subcomment.create(content: Faker::Food.description)
 end
 
-#Ajout id dans comments
+puts "Ajout id dans comments"
 5.times do |i|
 	Comment.find(i+1).update(user_id: rand(1..10))
 end
 
-#Ajout id dans link
+puts "Ajout id dans link"
 10.times do |i|
 	Link.find(i+1).update(user_id: rand(1..10))
 end
 
-#Ajout id dans subcomments
+puts "Ajout id dans subcomments"
 12.times do |i|
 	Subcomment.find(i+1).update(comment_id: rand(1..10))
 end
